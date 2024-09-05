5a18867c794a6aed1aa9a14c1bd30af7783a2b65
Miumaru-iniad <s1f102200637@iniad.org>
2024-09-06 01:53:54 +0900

    fix2

--- src/app/purchase/page.tsx
+++ src/app/purchase/page.tsx
@@ -1,31 +1,14 @@
-"use client";
-
-import { useEffect, useState } from 'react';
-import { useSearchParams } from 'next/navigation';
+// app/purchase/page.tsx
 import Link from 'next/link';
 
-export const dynamic = 'force-dynamic'; // この行を追加
-
 export default function PurchasePage() {
-  const searchParams = useSearchParams();
-  const [product, setProduct] = useState<string | null>(null);
-
-  useEffect(() => {
-    const productValue = searchParams.get('product');
-    setProduct(productValue);
-  }, [searchParams]);
-
-  if (!product) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <div>
       <h1>購入ありがとうございました</h1>
-      <p>{product} をご購入いただき、ありがとうございました。</p>
       <Link href="/">
         <a>製品一覧に戻る</a>
       </Link>
     </div>
   );
 }
+
