9f713634f2ffc8b195906e7dabff901ae03838bd
Miumaru-iniad <s1f102200637@iniad.org>
2024-09-06 03:22:52 +0900

    header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,31 +4,24 @@ import Image from 'next/image';
 
 export default function Header() {
   return (
-    <header style={{ position: 'relative', height: '200px' }}>
-      {/* 背景画像としてhead.jpgを表示 */}
-      <Image 
-        src="/images/head.jpg" 
-        alt="Header Image" 
-        layout="fill" 
-        objectFit="cover" 
-        quality={100} 
-        priority 
-      />
-      {/* ホームページへのリンク */}
-      <div style={{
-        position: 'absolute', 
-        top: '50%', 
-        left: '50%', 
-        transform: 'translate(-50%, -50%)',
-        color: 'white',
-        fontSize: '24px',
-        fontWeight: 'bold',
-        textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)'
-      }}>
-        <Link href="/">
-          <a>ホームページに戻る</a>
-        </Link>
-      </div>
+    <header style={{ backgroundColor: '#516753', padding: '10px' }}>
+      {/* ホームページにリンクされた画像 */}
+      <Link href="/">
+        <a>
+          <div style={{ maxHeight: '80px', overflow: 'hidden' }}>
+            <Image 
+              src="/images/head.jpg" 
+              alt="Header Image"
+              layout="intrinsic" // アスペクト比を保ちながら表示
+              width={0}          // 幅は制約しない
+              height={80}        // 縦幅のみ制限
+              style={{ objectFit: 'contain' }} 
+            />
+          </div>
+        </a>
+      </Link>
     </header>
   );
 }
+
+
