13a4528ee90d8ab26625757fd5f77bf9137b6f19
Miumaru-iniad <s1f102200637@iniad.org>
2024-09-06 01:19:18 +0900

    fix2

--- src/app/purchase/page.tsx
+++ src/app/purchase/page.tsx
@@ -1,19 +1,18 @@
-// app/purchase/page.tsx
-
 "use client";
 
-import { useEffect, useState, Suspense } from 'react';
+import { useEffect, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
 import Link from 'next/link';
+
+export const dynamic = 'force-dynamic'; // この行を追加
 
 export default function PurchasePage() {
   const searchParams = useSearchParams();
   const [product, setProduct] = useState<string | null>(null);
 
   useEffect(() => {
-    if (searchParams) {
-      setProduct(searchParams.get('product'));
-    }
+    const productValue = searchParams.get('product');
+    setProduct(productValue);
   }, [searchParams]);
 
   if (!product) {
@@ -21,14 +20,12 @@ export default function PurchasePage() {
   }
 
   return (
-    <Suspense fallback={<div>Loading purchase details...</div>}>
-      <div>
-        <h1>購入ありがとうございました</h1>
-        <p>{product} をご購入いただき、ありがとうございました。</p>
-        <Link href="/">
-          <a>製品一覧に戻る</a>
-        </Link>
-      </div>
-    </Suspense>
+    <div>
+      <h1>購入ありがとうございました</h1>
+      <p>{product} をご購入いただき、ありがとうございました。</p>
+      <Link href="/">
+        <a>製品一覧に戻る</a>
+      </Link>
+    </div>
   );
 }
